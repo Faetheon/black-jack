b7af6da455eb3c3d9140eed1ce19ebeaa354ee06
Nathan Hill <nateohill@gmail.com>
2020-05-13 21:07:45 -0500

    Initialized basic server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(`${__dirname}/../react/dist/`));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(`${__dirname}/../dist/`));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.search(bodyParser.json());
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../react/dist/index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 app.listen(PORT, () => {
